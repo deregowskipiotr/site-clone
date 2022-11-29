3e71a91d6e6a16f71ad8d418bbbf2207de30cf06
Piotr Deregowski <deregowskipiotr@interia.pl>
2022-11-29 16:53:55 +0100

    Add'ed info to Readme

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -34,7 +34,8 @@ const VideoDetail = () => {
           <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
             <ReactPlayer
               url={`https://www.youtube.com/watch?v=${id}`}
-              className="react-player controls"
+              className="react-player"
+              controls
             />
             <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
               {title}
